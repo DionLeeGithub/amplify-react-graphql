73e21c0a72dd20b0aa90b6fc9098e8b5cf43e1e8
Dion Lee <dion.lie123@gmail.com>
2023-06-17 14:36:15 +0700

    Navbar Unpog

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1>
+				Testpage for ALLYOURWHEELS WHEEEEEEEEEEEEEEEEEE
+			</h1>
+		</div>
+	);
+};
+
+export default About;
