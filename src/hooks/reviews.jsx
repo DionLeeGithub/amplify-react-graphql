d67d6056b1d0278fe97a655d23040a736d9ba6f9
Dion Lee <dion.lie123@gmail.com>
2023-07-03 15:52:32 +0700

    We Got Reviews!

--- src/hooks/reviews.jsx
+++ src/hooks/reviews.jsx
@@ -31,15 +31,15 @@ export default function Actuallyproperreviews() {
         modules={[FreeMode, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide><div className="ReviewText">★★★★★✩✩✩✩✩<br></br><i>"Wow pelek ku bisa terbang di langit yang biru gaes wkwkwkwk"</i><br></br><br></br>Pelek Master</div><img src={require('../Images/Pelak One.png')} /></SwiperSlide>
-        <SwiperSlide><div className="ReviewText">★★★★✩✩✩<br></br><i>"Gaes pelek ku kok bisa melayang sendiri ke rumah tetangga ya???"</i><br></br><br></br>Setan Pelek</div><img src={require('../Images/Pelak Two.png')} /></SwiperSlide>
-        <SwiperSlide><div className="ReviewText">★★★★★★★★★★★★★★★★★✩✩✩✩✩<br></br><i>"Gaes aku berhasil ngeheck bintang reviewnya, kerenz kanz? oiya peleknya bagus."</i><br></br><br></br>Hackerman6969</div><img src={require('../Images/Pelak Three.png')} /></SwiperSlide>
-        <SwiperSlide><div className="ReviewText">★★★✩✩✩<br></br><i>"Gaes keknya aku salah beli pelek, ini apaan?"</i><br></br><br></br>Pedagang Baso Allogio</div><img src={require('../Images/Bocchi.gif')} /></SwiperSlide>
-        <SwiperSlide><div className="ReviewText">★★★★★✩✩✩✩✩<br></br><i>"Wow pelek ku bisa terbang di langit yang biru gaes wkwkwkwk"</i><br></br><br></br>Pelek Master</div><img src={require('../Images/Pelak One.png')} /></SwiperSlide>
-        <SwiperSlide><div className="ReviewText">★★★★★✩✩✩✩✩<br></br><i>"Wow pelek ku bisa terbang di langit yang biru gaes wkwkwkwk"</i><br></br><br></br>Pelek Master</div><img src={require('../Images/Pelak One.png')} /></SwiperSlide>
-        <SwiperSlide><div className="ReviewText">★★★★★✩✩✩✩✩<br></br><i>"Wow pelek ku bisa terbang di langit yang biru gaes wkwkwkwk"</i><br></br><br></br>Pelek Master</div><img src={require('../Images/Pelak One.png')} /></SwiperSlide>
-        <SwiperSlide><div className="ReviewText">★★★★★✩✩✩✩✩<br></br><i>"Wow pelek ku bisa terbang di langit yang biru gaes wkwkwkwk"</i><br></br><br></br>Pelek Master</div><img src={require('../Images/Pelak One.png')} /></SwiperSlide>
-        <SwiperSlide><div className="ReviewText">★★★★★✩✩✩✩✩<br></br><i>"Wow pelek ku bisa terbang di langit yang biru gaes wkwkwkwk"</i><br></br><br></br>Pelek Master</div><img src={require('../Images/Pelak One.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★★★<br></br><i>"Lagi diracun sama @allyourwheels 😂😂😂"</i><br></br><br></br>ron.cx5ke</div><img src={require('../Images/Ron.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★★★<br></br><i>"tempat ku berlabuh @allyourwheels 🥰"</i><br></br><br></br>2gd.obs</div><img src={require('../Images/2gd.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★★★<br></br><i>"Akhirnya Si Candi Beres Juga dan langsung ganti sepatu.. untung aja @allyourwheels punya size yang cocok🥰🥰 "</i><br></br><br></br>gasz.mobilan</div><img src={require('../Images/Gaszm.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★★★<br></br><i>"Thx @allyourwheels . Kijang tua jadi punya sepatu baru …. 😂😂😂"</i><br></br><br></br>barong2kd</div><img src={require('../Images/Baro.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★★★<br></br><i>"Sepatu baru 😬😬"</i><br></br><br></br>b2drw</div><img src={require('../Images/B2d.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★★★<br></br><i>"Setelah 2 taun ga ganti velg🤧poll pokoknya @allyourwheels "</i><br></br><br></br>kodok.gk5</div><img src={require('../Images/Kodok.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★★★<br></br><i>"disakiti oleh si ZE, disembuhi oleh si TE <br></br>Special Thanks to @allyourwheels for the new shoes"</i><br></br><br></br>brovezel</div><img src={require('../Images/Bro.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★★★<br></br><i>"ganti sepatu ✌🏻 Thank you @allyourwheels"</i><br></br><br></br>vad3r.g5k</div><img src={require('../Images/Vad.png')} /></SwiperSlide>
+        <SwiperSlide><div className="ReviewText">★★★✩✩✩<br></br><i>"Welcoming Rocky, a.k.a Minion as our new daily driver. Hope this car lasts long & have a long journey with us✨😃 "</i><br></br><br></br>raoul.sava</div><img src={require('../Images/Raoul.png')} /></SwiperSlide>
       </Swiper>
     </>
   );
